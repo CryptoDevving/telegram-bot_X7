1d1f7263230d066f491a5d2a6abfd77915b82205
Adz1DoubleD <86771605+Adz1DoubleD@users.noreply.github.com>
2023-08-01 18:01:04 +0100

    Update times.py

--- data/times.py
+++ data/times.py
@@ -6,7 +6,7 @@ alert_time = 24
 referral_time = 6
 
 # COUNTDOWN
-countdown = datetime(2023, 6, 4, 15, 00, 00)
-countdown_title = "X7 Finance BETA Swap UI Go live"
-countdown_desc = "https://beta.x7.finance"
+countdown = datetime(2023, 9, 7, 00, 00, 00)
+countdown_title = "X7 Finance launch"
+countdown_desc = "https://app.x7.finance"
 countdown_command = "countdown"
