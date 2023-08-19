ef2f678c5bb8a9cf36b22c8f237d4a090e1b0d18
Adz1DoubleD <adz1doubled@gmail.com>
2023-08-19 20:09:05 +0100

    chore: add pair

---
+++ tokens/salt.py
@@ -0,0 +1,10 @@
+from tokens import TokensInfo
+
+
+info = TokensInfo(
+    name="snorkelz",
+    ca="0xb010f0a8eE6AFA6E04377d209277a9527a963Dd5",
+    pair="0x8013C2e185bEFAeFA6761093Bd6E123EfE98Db31",
+    chain="eth",
+    logo="https://www.dextools.io/resources/tokens/logos/ether/0xb010f0a8ee6afa6e04377d209277a9527a963dd5.jpg?1692338621491"
+)
