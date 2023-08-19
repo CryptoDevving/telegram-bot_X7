20b29bfe9e575228f13b7f532bdebead6a3d13f5
Adz1DoubleD <adz1doubled@gmail.com>
2023-08-19 16:43:32 +0100

    chore: add new pairs

---
+++ tokens/bpd.py
@@ -0,0 +1,10 @@
+from tokens import TokensInfo
+
+
+info = TokensInfo(
+    name="bpd",
+    ca="0xC81b0d02393a956234E24564805a896f14ad1250",
+    pair="0x4417d89cb2230b3a8f32180a3cbe0a48100e5491",
+    chain="eth",
+    logo="https://www.dextools.io/resources/tokens/logos/ether/0xc81b0d02393a956234e24564805a896f14ad1250.png?1692168833333"
+)
---
+++ tokens/snorkelz.py
@@ -0,0 +1,10 @@
+from tokens import TokensInfo
+
+
+info = TokensInfo(
+    name="snorkelz",
+    ca="0x104B9c2b4E707278B21f0cC8459e1B7596A315f6",
+    pair="0x7e81dfe4ac4a5d2b908576b8faba036529deed0f",
+    chain="eth",
+    logo="https://www.dextools.io/resources/tokens/logos/ether/0x104b9c2b4e707278b21f0cc8459e1b7596a315f6.png?1692430135017"
+)
