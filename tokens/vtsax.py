fbb4f794ee457781c3071f5a3d9663616b7be845
Adz1DoubleD <adz1doubled@gmail.com>
2023-08-19 17:23:52 +0100

    chore: add pair

---
+++ tokens/vtsax.py
@@ -0,0 +1,10 @@
+from tokens import TokensInfo
+
+
+info = TokensInfo(
+    name="vtsax",
+    ca="0xeD1cBedf2b53eb61aF50b4a06d21A1Ca636D420a",
+    pair="0xf6345dd5DeBCd71180ba1Ba0AD891AF6241bc5e1",
+    chain="eth",
+    logo=""
+)
