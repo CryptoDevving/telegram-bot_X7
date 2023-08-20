6dea1661b2bad20fcc8235662946f9e676201794
Adz1DoubleD <adz1doubled@gmail.com>
2023-08-20 23:21:23 +0100

    chore: add token

---
+++ tokens/donut.py
@@ -0,0 +1,10 @@
+from tokens import TokensInfo
+
+
+info = TokensInfo(
+    name="donut",
+    ca="0xC0F9bD5Fa5698B6505F643900FFA515Ea5dF54A9",
+    pair="0x4e04c2edFC65D6518BEAD91dFff4E5fC895cFF90",
+    chain="eth",
+    logo="https://www.dextools.io/resources/tokens/logos/3/ether/0xc0f9bd5fa5698b6505f643900ffa515ea5df54a9.png?1548234345"
+)
