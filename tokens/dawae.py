5c24a143471b77ae31d209e9608c8b582a633708
Adz1DoubleD <adz1doubled@gmail.com>
2023-08-22 08:24:32 +0100

    chore: add pair

---
+++ tokens/dawae.py
@@ -0,0 +1,10 @@
+from tokens import TokensInfo
+
+
+info = TokensInfo(
+    name="dawae",
+    ca="0x7DFF54CEcdfA2af69C95709AF2847A18D135fC3E",
+    pair="0xbc953fccbcc9e95dafb35d46992cee966aa972cd",
+    chain="eth",
+    logo=""
+)
