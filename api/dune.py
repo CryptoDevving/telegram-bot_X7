9b2ac21ff0ccabb526d551b3894739a173320489
Adz1DoubleD <adz1doubled@gmail.com>
2023-08-26 09:55:05 +0100

    fix: dune API key

--- api/dune.py
+++ api/dune.py
@@ -2,16 +2,14 @@ import os
 import random
 
 from requests import get, post
-from dotenv import load_dotenv
-
-
-def API_KEY():
-    api_key = random.choice(API_KEYS)
-    return api_key 
 
 API_KEYS = [os.getenv("DUNE_API_KEY"),os.getenv("DUNE_API_KEY_2"),os.getenv("DUNE_API_KEY_3"),os.getenv("DUNE_API_KEY_4")]
-HEADER = {"x-dune-api-key": API_KEY()}
 BASE_URL = "https://api.dune.com/api/v1/"
+
+
+def HEADER(): 
+    header = {"x-dune-api-key": random.choice(API_KEYS)}
+    return header
 
 
 def make_api_url(module, action, identifier):
@@ -21,19 +19,19 @@ def make_api_url(module, action, identifier):
 def execute_query(query_id, engine="small"):
     response = post(
         make_api_url("query", "execute", query_id),
-        headers=HEADER,
+        headers=HEADER(),
         params={"performance": engine},
     )
     return response.json()["execution_id"]
 
 
 def get_query_status(execution_id):
-    return get(make_api_url("execution", "status", execution_id), headers=HEADER)
+    return get(make_api_url("execution", "status", execution_id), headers=HEADER())
 
 
 def get_query_results(execution_id):
-    return get(make_api_url("execution", "results", execution_id), headers=HEADER)
+    return get(make_api_url("execution", "results", execution_id), headers=HEADER())
 
 
 def cancel_query_execution(execution_id):
-    return get(make_api_url("execution", "cancel", execution_id), headers=HEADER)
+    return get(make_api_url("execution", "cancel", execution_id), headers=HEADER())
