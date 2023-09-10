298b09e4d74cb7bf8aa01d25753e945538eb9a58
Adz1DoubleD <adz1doubled@gmail.com>
2023-09-10 13:13:37 +0100

    fix: scan text

--- scanner_poly.py
+++ scanner_poly.py
@@ -234,7 +234,6 @@ async def new_pair(event):
         f'Supply: {"{:0,.0f}".format(supply)} ({info[0]["decimals"]} Decimals)\n\n'
         f"{pool_text}\n\n"
         f"{liquidity_text}\n\n"
-        f"SCAN:\n"
         f"{status}",
         font=myfont,
         fill=(255, 255, 255),
@@ -254,7 +253,6 @@ async def new_pair(event):
             f'Supply: {"{:0,.0f}".format(supply)} ({info[0]["decimals"]} Decimals)\n\n'
             f"{pool_text}\n\n"
             f"{liquidity_text}\n\n"
-            f"SCAN:\n"
             f"{status}",
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
