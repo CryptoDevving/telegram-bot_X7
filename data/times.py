cd3842dea36eae4949060598f880bf6ab6ba4dd9
Adz1DoubleD <adz1doubled@gmail.com>
2023-10-14 22:36:06 +0100

    fix

--- data/times.py
+++ data/times.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # AUTO TIMES SECONDS
 def random_button_time():
-    time = random.randint(1, 5)
+    time = random.randint(1, 44444)
     return time
 
 first_button_time = random_button_time()
