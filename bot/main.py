e262c924cef829729abf5a9d7531d4fd70a52847
Adz1DoubleD <adz1doubled@gmail.com>
2023-10-21 16:06:25 +0100

    reset leadeboard

--- bot/main.py
+++ bot/main.py
@@ -155,6 +155,9 @@ async def clicks_function(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
                 click_message = "🎉🎉 This is their first button click! 🎉🎉"
             elif clicks % 10 == 0:
                 click_message = f"🎉🎉 They been the fastest Pioneer {clicks} times! 🎉🎉"
+
+            elif clicks == 20:
+                click_message = f"🎉🎉 They been the fastest Pioneer {clicks} times and won a X7 Finance Pioneer NFT! 🎉🎉"
             else:
                 click_message = f"They have been the fastest Pioneer {clicks} times!"
 
