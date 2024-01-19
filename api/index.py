d3399d00db3069fce78a39b586788dd50b7cd70c
Adz1DoubleD <adz1doubled@gmail.com>
2024-01-19 21:46:02 +0000

    improved gas estimations

--- api/index.py
+++ api/index.py
@@ -627,22 +627,27 @@ async def burn_x7r(amount):
             + hex(amount_to_send_wei)[2:].rjust(64, '0')
         )
 
+        nonce = w3.eth.get_transaction_count(sender_address)
+        gas = w3.eth.estimate_gas({
+            'from': sender_address,
+            'to': token_contract_address,
+            'data': token_transfer_data,
+        })
+
         transaction = {
             'from': sender_address,
             'to': token_contract_address,
             'data': token_transfer_data,
-            'gasPrice': w3.to_wei('50', 'gwei'),
+            'gasPrice': w3.to_wei(w3.eth.gas_price / 1e9 , 'gwei'),
+            'gas': gas,
+            'nonce': nonce,
         }
-        
-        gas_estimate = w3.eth.estimate_gas(transaction)
-        nonce = w3.eth.get_transaction_count(sender_address)
-        transaction['gas'] = gas_estimate
-        transaction['nonce'] = nonce
+
         signed_transaction = w3.eth.account.sign_transaction(transaction, sender_private_key)
         tx_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
         return f"{amount} X7R Burnt\n\n{url.ether_tx}{tx_hash.hex()}"
     except Exception as e:
-            return f'Error burning X7R: {e}'
+        return f'Error burning X7R: {e}'
 
 
 def datetime_to_timestamp(datetime_str):
