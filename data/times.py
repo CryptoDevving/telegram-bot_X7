45e7d7e4b5e771ab7f0acbea7524e2985aa75177
Adz1DoubleD <adz1doubled@gmail.com>
2024-01-29 22:45:11 +0000

    dont click me test

--- data/times.py
+++ data/times.py
@@ -12,7 +12,7 @@ restart_time = datetime.now().timestamp()
 button_time = None
 
 def dont_random_button_time():
-    time = random.randint(3600, 3601)
+    time = random.randint(300, 301)
     return time
 
 dont_first_button_time = dont_random_button_time()
